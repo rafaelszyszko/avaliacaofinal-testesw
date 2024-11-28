643398b9046cecea374dc68507b733a07a68eb4e
rafaelszyszko <rafabszyszko2004@gmail.com>
2024-11-27 23:34:56 -0300

    Envio teste 3

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://parabank.parasoft.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implement event listeners if needed
     },
   },
 });
